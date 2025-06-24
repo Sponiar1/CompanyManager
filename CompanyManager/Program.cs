6f783df251c3f406e0c0884d537dfe3e8b37a403
Martin <sponiarmartin2000@gmail.com>
2025-06-24 18:45:17 +0200

    Changed Launch Url To Scalar

--- CompanyManager/Program.cs
+++ CompanyManager/Program.cs
@@ -19,12 +19,12 @@ builder.Services.AddScoped<CompanyService>();
 builder.Services.AddScoped<DivisionService>();
 builder.Services.AddScoped<DepartmentService>();
 builder.Services.AddScoped<ProjectService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger(options =>
